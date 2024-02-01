596e8d8ed5c306436e376a913e7f9cecd2bedbda
shivangisaraf <shivangisaraf15@gmail.com>
2024-02-01 23:07:25 +0530

    add

---
+++ src/OOPs/main.java
@@ -0,0 +1,13 @@
+package OOPs;
+
+public class main {
+    public static void main(String[] args) {
+        human shivangi=new human(20,"shivangi",1000,false);
+        human jadoo=new human(19,"jadoo",1000,false);
+        human ladoo=new human(21,"ladoo",1000,false);
+
+        System.out.println(human.population);
+        System.out.println(human.population);
+        System.out.println(human.population);
+    }
+}
---
+++ src/inheritance/main.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class main {
+    public static void main(String[] args) {
+//        Box box=new Box(4 ,5,6);
+////        Box box2=new Box(box);
+//        System.out.println(box.l +" " + box.h +" " + box.w);
+
+        BoxWeight box3=new BoxWeight();
+        System.out.println(box3.h + " " + box3.weight);
+    }
+}
